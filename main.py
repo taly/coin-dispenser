fa95f1e67fddaa1e81a7171bb452a264938671ab
Taly Rabinerson <tafitaf@gmail.com>
2016-06-26 14:20:02 +0000

    Algorithm optimizations

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import sched
 import arm_controller
 import dispenser_client
 
-POLLING_INTERVAL = 5
+POLLING_INTERVAL = 1
 PAUSE_BETWEEN_COINS_SEC = 2
 DEBUG_FMT = "%H:%M:%S:%f"
-
-#working = False
-#coins_to_dispense = 0
 
 def setup_arm():
     arm_controller.setup()
@@ -20,55 +17,21 @@ def setup_arm():
     arm_controller.off()
 
 
-def dispense_remaining_coins():
-    global working
-    global coins_to_dispense
-
-    if working:
-        return
-
-    working = True
-    while coins_to_dispense > 0:
-        arm_controller.on()
-        debug("Dispensing...")
-        arm_controller.dispense_coin()
-        coins_to_dispense -= 1
-        sleep_time = arm_controller.get_total_dispensing_time()
-        if coins_to_dispense > 1:
-            sleep_time += PAUSE_BETWEEN_COINS_SEC
-        time.sleep(sleep_time)
-        debug("\tDispensed, coins left: %d" % coins_to_dispense)
-    
-    working = False
-    arm_controller.off()
-
-
-def poll(scheduler):
-    global coins_to_dispense
-    scheduler.enter(POLLING_INTERVAL, 1, poll, (scheduler,))    
-
-    debug("Polling...")
-    to_dispense = dispenser_client.coins_to_dispense()
-    coins_to_dispense += to_dispense
-    debug("\tPolled, got: %d, total: %d" % (to_dispense, coins_to_dispense))
-    dispense_remaining_coins()
-
-
 def start_poll_loop():
-    #print "Polling every %d secs..." % POLLING_INTERVAL
-
     while True:
+        debug("Polling...")
         to_dispense = dispenser_client.coins_to_dispense()
+        debug("Got: %d" % to_dispense)
         while to_dispense > 0:
+            debug("\tDispensing coin...")
             arm_controller.dispense_coin()
             sleep_time = arm_controller.get_total_dispensing_time()
             if to_dispense > 1:
                 sleep_time += PAUSE_BETWEEN_COINS_SEC
             time.sleep(sleep_time)
-
-    #scheduler = sched.scheduler(time.time, time.sleep)
-    #scheduler.enter(1, 1, poll, (scheduler,))
-    #scheduler.run()
+            to_dispense -= 1
+            debug("\t\tDispensed")
+        time.sleep(POLLING_INTERVAL)
 
 
 def debug(msg):
